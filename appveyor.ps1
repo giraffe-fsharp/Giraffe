3422f03e715ab9e57f502bd5e281fe0ffa24bfeb
Dustin Moris Gorski <dustinmoris@gmail.com>
2017-02-19 10:20:13 +0000

    Moved things to appveyor.ps1 and trying to update appveyor build numbers during build time

---
+++ appveyor.ps1
@@ -0,0 +1,40 @@
+
+#-------------------------------
+# Installation
+#-------------------------------
+
+Write-Host "Downloading latest .NET Core SDK..."
+
+Invoke-WebRequest "https://go.microsoft.com/fwlink/?linkid=841686" -OutFile "dotnet-core-sdk.exe"
+
+Write-Host "Installing .NET Core SDK..."
+
+Invoke-Command -ScriptBlock { ./dotnet-core-sdk.exe /S /v/qn }
+
+Write-Host "Installation succeeded." -ForegroundColor Green
+
+#-------------------------------
+# Update AppVeyor Build version
+#-------------------------------
+
+Write-Host "Parsing project file..."
+
+[xml]$proj = Get-Content -Path "./src/AspNetCore.Lambda/AspNetCore.Lambda.fsproj"
+
+$versionPrefix = $proj.Project.PropertyGroup.VersionPrefix
+
+Write-Host "Version prefix: $versionPrefix"
+
+$version = $versionPrefix + $env:APPVEYOR_BUILD_NUMBER
+
+Write-Host "Updating AppVeyor build version to $version."
+
+Update-AppveyorBuild -Version $version
+
+#-------------------------------
+# Update AppVeyor Build version
+#-------------------------------
+
+Write-Host "Launching build.cmd..."
+
+./build.cmd
--- install.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "Downloading latest .NET Core SDK..."
-
-Invoke-WebRequest "https://go.microsoft.com/fwlink/?linkid=841686" -OutFile "dotnet-core-sdk.exe"
-
-Write-Host "Installing .NET Core SDK..."
-
-Invoke-Command -ScriptBlock { ./dotnet-core-sdk.exe /S /v/qn }
-
-Write-Host "Installation succeeded." -ForegroundColor Green
