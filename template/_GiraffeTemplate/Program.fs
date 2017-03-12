84157b8f9f269c907841144a9ffb28fdb7857a7f
David Sinclair <David Sinclair>
2017-03-12 13:04:21 +0100

    A Giraffe template for dotnet new

---
+++ template/_GiraffeTemplate/Program.fs
@@ -0,0 +1,68 @@
+module _GiraffeTemplate
+
+open System
+open System.IO
+open System.Security.Claims
+open System.Collections.Generic
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open Giraffe.HttpHandlers
+open Giraffe.Middleware
+open Models
+
+// ---------------------------------
+// Error handler
+// ---------------------------------
+
+let errorHandler (ex : Exception) (ctx : HttpHandlerContext) =
+    let loggerFactory = ctx.Services.GetService<ILoggerFactory>()
+    let logger = loggerFactory.CreateLogger "ErrorHandlerLogger"
+    logger.LogError(EventId(0), ex, "An unhandled exception has occurred while executing the request")
+    ctx |> (clearResponse >=> setStatusCode 500 >=> text ex.Message)
+
+// ---------------------------------
+// Web app
+// ---------------------------------
+
+let webApp = 
+    choose [
+        GET >=>
+            choose [
+                route  "/"           >=> razorView "Hello.cshtml" { Text = "Hello, Giraffe world!" }
+            ]
+
+        setStatusCode 404 >=> text "Not Found" ]
+
+// ---------------------------------
+// Main
+// ---------------------------------
+
+let configureApp (app : IApplicationBuilder) = 
+    app.UseGiraffeErrorHandler(errorHandler)
+    app.UseGiraffe(webApp)
+
+let configureServices (services : IServiceCollection) =
+    let sp  = services.BuildServiceProvider()
+    let env = sp.GetService<IHostingEnvironment>()
+    let viewsFolderPath = Path.Combine(env.ContentRootPath, "Views")
+
+    services.AddRazorEngine(viewsFolderPath) |> ignore
+
+let configureLogging (loggerFactory : ILoggerFactory) =
+    loggerFactory.AddConsole(LogLevel.Trace).AddDebug() |> ignore
+
+[<EntryPoint>]
+let main argv =                                
+    let host =
+        WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .Configure(Action<IApplicationBuilder> configureApp)
+            .ConfigureServices(Action<IServiceCollection> configureServices)
+            .ConfigureLogging(Action<ILoggerFactory> configureLogging)
+            .Build()
+    host.Run()
+    0
