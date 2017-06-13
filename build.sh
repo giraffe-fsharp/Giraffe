6f37f03e4a77196456975cc7b104c255747c9bbd
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2017-06-12 20:26:16 -0400

    Added .travis-ci for mono/dotnet core linux builds

--- build.sh
+++ build.sh
@@ -4,15 +4,15 @@ set -eu
 
 export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
 
-# dotnet restore src/Giraffe
-# dotnet build src/Giraffe
+dotnet restore src/Giraffe
+dotnet build src/Giraffe
 
-# dotnet restore tests/Giraffe.Tests
-# dotnet build tests/Giraffe.Tests
+dotnet restore tests/Giraffe.Tests
+dotnet build tests/Giraffe.Tests
 
-# pushd tests/Giraffe.Tests
-# dotnet xunit 
-# popd
+pushd tests/Giraffe.Tests
+dotnet xunit 
+popd
 
 dotnet restore samples/SampleApp/SampleApp
 dotnet build samples/SampleApp/SampleApp
