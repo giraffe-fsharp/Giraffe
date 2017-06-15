293e450faeff5044a800101d90a2eaef8cbce41f
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2017-06-15 11:54:52 -0400

    only running dotnet test on netcoreapp1.1 on linux for now

--- build.sh
+++ build.sh
@@ -9,17 +9,14 @@ dotnet build src/Giraffe
 
 dotnet restore tests/Giraffe.Tests
 dotnet build tests/Giraffe.Tests
-
-pushd tests/Giraffe.Tests
-dotnet xunit 
-popd
+# Ignoring net4x because dotnet test doesn't support mono yet https://github.com/Microsoft/vstest/issues/679
+dotnet test -f netcoreapp1.1 tests/Giraffe.Tests/Giraffe.Tests.fsproj
 
 dotnet restore samples/SampleApp/SampleApp
 dotnet build samples/SampleApp/SampleApp
 
 dotnet restore samples/SampleApp/SampleApp.Tests
 dotnet build samples/SampleApp/SampleApp.Tests
-pushd samples/SampleApp/SampleApp.Tests/
-dotnet xunit 
-popd
+dotnet test -f netcoreapp1.1 samples/SampleApp/SampleApp.Tests/SampleApp.Tests.fsproj
+
 
