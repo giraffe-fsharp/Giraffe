329616b3aad6cb487ab7632b8ec8b8da481393f5
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-12-30 14:43:10 +0100

    cleanuo

--- tests/Giraffe.Tests/WebSocketTests.fs
+++ tests/Giraffe.Tests/WebSocketTests.fs
@@ -15,38 +15,38 @@ open RequestErrors
 open Giraffe.TokenRouter
 
 
-let echoSocket (connectionManager : ConnectionManager) token =
+let echoSocket (connectionManager : ConnectionManager) cancellationToken =
     connectionManager.CreateSocket(
         (fun _ref -> task { return true }),
-        (fun ref data -> SendText ref.WebSocket data token),
-        token)
+        (fun ref data -> SendText ref.WebSocket data cancellationToken),
+        cancellationToken)
 
-let webApp cm token =
+let webApp cm cancellationToken =
     let notfound = NOT_FOUND "Page not found"
    
     router notfound [
        GET [
-           route "/echo" (echoSocket cm token) 
+           route "/echo" (echoSocket cm cancellationToken) 
        ]
     ]
 
-let configure (app : IApplicationBuilder,cm,token) =
+let configure (app : IApplicationBuilder,cm,cancellationToken) =
     app
       .UseWebSockets()
-      .UseGiraffe (webApp cm token)
+      .UseGiraffe (webApp cm cancellationToken)
 
-let createClient (server:TestServer,token) = task {
+let createClient (server:TestServer,cancellationToken) = task {
     let wsClient = server.CreateWebSocketClient()
     let url = server.BaseAddress.AbsoluteUri + "echo"
-    return! wsClient.ConnectAsync(Uri(url), token)
+    return! wsClient.ConnectAsync(Uri(url), cancellationToken)
 }
 
-let receiveText (websocket:WebSocket,token) = task {
+let receiveText (websocket:WebSocket,cancellationToken) = task {
     let moveBuffer (buffer: ArraySegment<'T>) count =
         ArraySegment(buffer.Array, buffer.Offset + count, buffer.Count - count)
 
     let rec receive receivedBytes = task {
-        let! result = websocket.ReceiveAsync(receivedBytes, token)
+        let! result = websocket.ReceiveAsync(receivedBytes, cancellationToken)
         let currentBuffer = moveBuffer receivedBytes result.Count
         if result.EndOfMessage then
             return ConvertToMsg currentBuffer.Array
@@ -65,15 +65,15 @@ let createServer(cm,token) =
 
 [<Fact>]
 let ``Simple Echo Test`` () = task {
-    let token = CancellationToken.None
+    let cancellationToken = CancellationToken.None
     let cm = ConnectionManager()
 
-    use server = createServer (cm,token)
-    let! wsClient = createClient(server, token)
+    use server = createServer (cm, cancellationToken)
+    let! wsClient = createClient(server, cancellationToken)
 
     let expected = "Hello"
-    let! _ = SendText wsClient expected token
-    let! actual = receiveText (wsClient,token)
+    let! _ = SendText wsClient expected cancellationToken
+    let! actual = receiveText (wsClient, cancellationToken)
 
     Assert.Equal(expected,actual)
 }
@@ -85,13 +85,13 @@ let ``Simple Echo Test`` () = task {
 [<InlineData(100)>]
 [<InlineData(1000)>]
 let ``Can connect some clients`` (n:int) = task {
-    let token = CancellationToken.None
+    let cancellationToken = CancellationToken.None
     let cm = ConnectionManager()
 
-    use server = createServer (cm,token)
+    use server = createServer (cm,cancellationToken)
     let! clients =
         [1..n]
-        |> List.map (fun _ -> createClient(server,token))
+        |> List.map (fun _ -> createClient(server,cancellationToken))
         |> Task.WhenAll
 
     Assert.Equal(n,clients.Length)
@@ -109,22 +109,22 @@ let ``Can connect some clients`` (n:int) = task {
 [<InlineData(100)>]
 [<InlineData(1000)>]
 let ``Broadcast Test`` (n:int) = task {
-    let token = CancellationToken.None
+    let cancellationToken = CancellationToken.None
     let cm = ConnectionManager()
 
-    use server = createServer (cm,token)
+    use server = createServer (cm,cancellationToken)
     let! clients =
         [1..n]
-        |> List.map (fun _ -> createClient(server,token))
+        |> List.map (fun _ -> createClient(server,cancellationToken))
         |> Task.WhenAll
    
     let expected = "Hello"
-    let! sent = cm.SendToAll(expected,token)
+    let! sent = cm.SendToAll(expected,cancellationToken)
     Assert.Equal(n,sent)
 
     let! results = 
         clients
-        |> Seq.map (fun ws -> receiveText (ws,token))
+        |> Seq.map (fun ws -> receiveText (ws,cancellationToken))
         |> Task.WhenAll
 
     for x in results do
