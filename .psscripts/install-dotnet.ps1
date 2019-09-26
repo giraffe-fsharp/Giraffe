19d354b7868aa4e8d8f68826fc5a1deec25dba9a
Dustin Moris Gorski <dustinmoris@gmail.com>
2019-09-26 18:41:25 +0100

    Made a change to install latest SDKs
    Installing latest SDKs on build images

--- .psscripts/install-dotnet.ps1
+++ .psscripts/install-dotnet.ps1
@@ -4,51 +4,23 @@
 
 param
 (
-    [switch] $ForceUbuntuInstall
+    [string] $SdkVersions
 )
 
 $ErrorActionPreference = "Stop"
 
 Import-module "$PSScriptRoot\build-functions.ps1" -Force
 
-if ($ForceUbuntuInstall.IsPresent)
-{
-    $desiredSdk    = Get-DesiredSdk
-    $versionParts  = $desiredSdk.Split(".")
-    $majorMinorVer = $versionParts[0] + "." + $versionParts[1]
-    $ubuntuVersion = Get-UbuntuVersion
+# Get desired SDKs from argument
+$desiredSDKs = $SdkVersions.Split(",") | % { $_.Trim() }
 
-    Write-Host "Ubuntu version: $ubuntuVersion"
-    Install-NetCoreSdkForUbuntu $ubuntuVersion $majorMinorVer
-    return
+foreach($desiredSDK in $desiredSDKs)
+{
+    Write-Host "Attempting to download and install the .NET SDK $desiredSDK..."
+    $sdkZipPath = Get-NetCoreSdkFromWeb $desiredSDK
+    Install-NetCoreSdkFromArchive $sdkZipPath
 }
 
-# Rename the global.json before making the dotnet --version call
-# This will prevent AppVeyor to fail because it might not find
-# the desired SDK specified in the global.json
-$globalJson = Get-Item "$PSScriptRoot\..\global.json"
-Rename-Item -Path $globalJson.FullName -NewName "global.json.bak" -Force
-
-# Get the current .NET Core SDK version
-$currentSdk = dotnet-version
-
-# After we established the current installed .NET SDK we can put the global.json back
-Rename-Item -Path ($globalJson.FullName + ".bak") -NewName "global.json" -Force
-
-$desiredSdk = Get-DesiredSdk
-
-if ($desiredSdk -eq $currentSdk)
-{
-    Write-Host "The current .NET SDK matches the project's desired .NET SDK: $desiredSDK" -ForegroundColor Green
-    return
-}
-
-Write-Host "The current .NET SDK ($currentSdk) doesn't match the project's desired .NET SDK ($desiredSdk)." -ForegroundColor Yellow
-
-Write-Host "Attempting to download and install the correct .NET SDK..."
-
-$sdkZipPath = Get-NetCoreSdkFromWeb $desiredSdk
-Install-NetCoreSdkFromArchive $sdkZipPath
-
-Write-Host ".NET SDK installation complete." -ForegroundColor Green
+Write-Host ".NET SDK installations complete." -ForegroundColor Green
+dotnet-info
 dotnet-version
