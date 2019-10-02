4c9db4fae78a876281d4d8e5e006d30ce077e191
Dustin Moris Gorski <dustinmoris@gmail.com>
2019-10-02 22:48:17 +0100

    Got route and routef working
    First version of routef handler with new Endpoint Routing

---
+++ samples/EndpointRoutingApp/EndpointRoutingApp/Program.fs
@@ -0,0 +1,49 @@
+﻿open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+open Giraffe.EndpointRouting
+
+let handler1 : HttpHandler =
+    fun (_ : HttpFunc) (ctx : HttpContext) ->
+        ctx.WriteTextAsync "Hello World"
+
+let handler2 (firstName : string, age : int) : HttpHandler =
+    fun (_ : HttpFunc) (ctx : HttpContext) ->
+        sprintf "Hello %s, you are %i years old." firstName age
+        |> ctx.WriteTextAsync
+
+let handler3 (a : string, b : string, c : string, d : int) : HttpHandler =
+    fun (_ : HttpFunc) (ctx : HttpContext) ->
+        sprintf "Hello %s %s %s %i" a b c d
+        |> ctx.WriteTextAsync
+
+let routes =
+    [
+        route "/" handler1
+        routef "/%s/%i" handler2
+        routef "/%s/%s/%s/%i" handler3
+    ]
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseRouter(fun r -> r.MapGiraffe(routes))
+    |> ignore
+
+let configureServices (services : IServiceCollection) =
+    services
+        .AddRouting()
+        .AddGiraffe()
+    |> ignore
+
+[<EntryPoint>]
+let main args =
+    WebHost
+        .CreateDefaultBuilder(args)
+        .UseKestrel()
+        .Configure(configureApp)
+        .ConfigureServices(configureServices)
+        .Build()
+        .Run()
+    0
--- src/Giraffe/EndpointRouting.fs
+++ src/Giraffe/EndpointRouting.fs
@@ -1,33 +1,134 @@
-[<AutoOpen>]
 module Giraffe.EndpointRouting
 
+open System
 open System.Threading.Tasks
 open Microsoft.AspNetCore.Http
-open FSharp.Control.Tasks.V2
-
 open Microsoft.AspNetCore.Routing
+open Microsoft.FSharp.Reflection
+open FSharp.Core
+open FSharp.Control.Tasks.V2.ContextInsensitive
 
-let private convertToHttpFunc (handler : HttpHandler) =
-    handler (Some >> Task.FromResult)
+module private RouteTemplateBuilder =
+    let private getConstraint (i : int) (c : char) =
+        // Route Constraint Docs:
+        // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/routing?view=aspnetcore-3.0#route-constraint-reference
+        (dict [
+            'b', sprintf "{b%i:int}" i          // bool
+            'c', sprintf "{c%i:length(1)}" i    // char
+            's', sprintf "{s%i}" i              // string
+            'i', sprintf "{i%i:int}" i          // int
+            'd', sprintf "{d%i:long}" i         // int64
+            'f', sprintf "{f%i:double}" i       // float
+            'O', sprintf "{O%i}" i              // ToDo: Guid
+            'u', sprintf "{u%i}" i              // ToDo: uint64
+        ]).[c]
 
-let private convertToRequestDelegate (func : HttpFunc) =
-    fun (ctx : HttpContext) ->
-        task {
-            let! result = func ctx
-            match result with
-            | None   -> () // Shall we return here a HTTP 422 Unprocessable Entity?
-            | Some _ -> ()
-            return ()
-        } :> Task
+    let convertToRouteTemplate (path : PrintfFormat<_,_,_,_, 'T>) =
+        let rec convert (i : int) (chars : char list) =
+            match chars with
+            | '%' :: '%' :: tail ->
+                let template, formatChars = convert i tail
+                "%" + template, formatChars
+            | '%' :: c :: tail ->
+                let template, formatChars = convert (i + 1) tail
+                let placeholder = getConstraint i c
+                placeholder + template, c :: formatChars
+            | c :: tail ->
+                let template, formatChars = convert i tail
+                c.ToString() + template, formatChars
+            | [] -> "", []
+
+        path.Value
+        |> List.ofSeq
+        |> convert 0
+
+module private RequestDelegateBuilder =
+
+    let private parsers =
+        let parseBool     (s : string) = bool.Parse s
+        let decodeSlashes (s : string) = s.Replace("%2F", "/").Replace("%2f", "/")
+        let parseGuid     (s : string) =
+            match s.Length with
+            | 22 -> ShortGuid.toGuid s
+            | _  -> Guid s
+
+        dict [
+            'b', parseBool           >> box  // bool
+            'c', char                >> box  // char
+            's', decodeSlashes       >> box  // string
+            'i', int32               >> box  // int
+            'd', int64               >> box  // int64
+            'f', float               >> box  // float
+            'O', parseGuid           >> box  // Guid
+            'u', ShortId.toUInt64    >> box  // uint64
+        ]
+
+    let private convertToTuple (formatChars : char list) (routeData : RouteData) =
+        let values =
+            (routeData.Values.Values, formatChars)
+            ||> Seq.map2 (fun v c ->
+                let parser = parsers.[c]
+                let value  = parser (v.ToString())
+                value)
+            |> Seq.toArray
+
+        let result =
+            match values.Length with
+            | 1 -> values.[0]
+            | _ ->
+                let types =
+                    values
+                    |> Array.map (fun v -> v.GetType())
+                let tupleType = FSharpType.MakeTupleType types
+                FSharpValue.MakeTuple(values, tupleType)
+        result
+
+    let private wrapDelegate f = new RequestDelegate(f)
+
+    let private handleResult result =
+        match result with
+        | None   -> () // Shall we return here a HTTP 422 Unprocessable Entity?
+        | Some _ -> ()
+
+    let createRequestDelegate (handler : HttpHandler) =
+        fun (ctx : HttpContext) ->
+            task {
+                let! result = handler earlyReturn ctx
+                return handleResult result
+            } :> Task
+        |> wrapDelegate
+
+    let createTokenizedRequestDelegate (formatChars : char list) (tokenizedHandler : 'T -> HttpHandler) =
+        fun (ctx : HttpContext) ->
+            task {
+                let tuple =
+                    ctx.GetRouteData()
+                    |> convertToTuple formatChars
+                    :?> 'T
+                let! result = tokenizedHandler tuple earlyReturn ctx
+                return handleResult result
+            } :> Task
+        |> wrapDelegate
+
+// ---------------------------
+// Overriding Handlers
+// ---------------------------
+
+let route path handler =
+    path, RequestDelegateBuilder.createRequestDelegate handler
+
+let routef (path : PrintfFormat<_,_,_,_, 'T>) (routeHandler : 'T -> HttpHandler) =
+    let template, chars = RouteTemplateBuilder.convertToRouteTemplate path
+    let requestDelegate = RequestDelegateBuilder.createTokenizedRequestDelegate chars routeHandler
+    template, requestDelegate
+
+// ---------------------------
+// Convenience Handlers
+// ---------------------------
 
 type IRouteBuilder with
-    member private this.MapGiraffeGetRoute (path : string, func : HttpFunc) =
-        let requestDelegate = convertToRequestDelegate func
-        this.MapGet(path, requestDelegate)
-        |> ignore
 
-    member this.MapGiraffe (routes : (string * HttpHandler) list) =
+    member this.MapGiraffe (routes : (string * RequestDelegate) list) =
         routes
-        |> List.map(fun (p, h) -> (p, convertToHttpFunc h))
-        |> List.iter(this.MapGiraffeGetRoute)
-        this
+        |> List.iter(fun (routeTemplate, requestDelegate) ->
+            this.MapGet(routeTemplate, requestDelegate) |> ignore)
