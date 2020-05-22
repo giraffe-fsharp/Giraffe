9b91e3029d73d7f03cc7543ab402571e2f93af52
Stuart Lang <stuart.lang@just-eat.com>
2020-05-22 21:22:18 +0100

    Add System.Text.Json support

--- src/Giraffe/Serialization.fs
+++ src/Giraffe/Serialization.fs
@@ -8,6 +8,7 @@ open Microsoft.IO
 
 [<AutoOpen>]
 module Json =
+    open System
     open System.IO
     open System.Text
     open System.Threading.Tasks
@@ -121,6 +122,43 @@ module Json =
                     use jsonTextReader = new JsonTextReader(streamReader)
                     return serializer.Deserialize<'T>(jsonTextReader)
                 }
+
+    open System.Text.Json
+
+    /// **Description**
+    ///
+    /// The `SystemTextJsonSerializer` is an alternaive `IJsonSerializer` in Giraffe.
+    ///
+    /// It uses `System.Text.Json` as the underlying JSON serializer to (de-)serialize
+    /// JSON content. For support of F# unions and records, look at https://github.com/Tarmil/FSharp.SystemTextJson
+    /// which plugs into this serializer.
+    ///
+    type SystemTextJsonSerializer (options: JsonSerializerOptions) =
+
+        static member DefaultOptions =
+           JsonSerializerOptions(
+               PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+           )
+
+        interface IJsonSerializer with
+            member __.SerializeToString (x : 'T) =
+                JsonSerializer.Serialize(x,  options)
+
+            member __.SerializeToBytes (x : 'T) =
+                JsonSerializer.SerializeToUtf8Bytes(x, options)
+
+            member __.SerializeToStreamAsync (x : 'T) (stream : Stream) =
+                JsonSerializer.SerializeAsync(stream, x, options)
+
+            member __.Deserialize<'T> (json : string) : 'T =
+                JsonSerializer.Deserialize<'T>(json, options)
+
+            member __.Deserialize<'T> (bytes : byte array) : 'T =
+                JsonSerializer.Deserialize<'T>(Span<_>.op_Implicit(bytes.AsSpan()), options)
+
+            member __.DeserializeAsync<'T> (stream : Stream) : Task<'T> =
+                JsonSerializer.DeserializeAsync<'T>(stream, options).AsTask()
+
 // ---------------------------
 // XML
 // ---------------------------
