5bf0b040fd3999d875efa55241e13ab91779233e
Oliver Rösner <belancr@gmail.com>
2023-07-05 14:05:11 +0200

    RFC-compliant Content-Length handling for 1xx, 204 and 205 responses and CONNECT requests

--- src/Giraffe/Helpers.fs
+++ src/Giraffe/Helpers.fs
@@ -34,3 +34,44 @@ module Helpers =
             use reader = new StreamReader(filePath)
             return! reader.ReadToEndAsync()
         }
+
+    /// <summary>
+    /// Utility function for matching 1xx HTTP status codes.
+    /// </summary>
+    /// <param name="statusCode">The HTTP status code.</param>
+    /// <returns>Returns true if the status code is between 100 and 199.</returns>
+    let is1xxStatusCode (statusCode : int) =
+        100 <= statusCode && statusCode <= 199
+
+    /// <summary>
+    /// Utility function for matching 2xx HTTP status codes.
+    /// </summary>
+    /// <param name="statusCode">The HTTP status code.</param>
+    /// <returns>Returns true if the status code is between 200 and 299.</returns>
+    let is2xxStatusCode (statusCode : int) =
+        200 <= statusCode && statusCode <= 299
+
+    /// <summary>
+    /// Utility function for matching 3xx HTTP status codes.
+    /// </summary>
+    /// <param name="statusCode">The HTTP status code.</param>
+    /// <returns>Returns true if the status code is between 300 and 399.</returns>
+    let is3xxStatusCode (statusCode : int) =
+        300 <= statusCode && statusCode <= 399
+
+    /// <summary>
+    /// Utility function for matching 4xx HTTP status codes.
+    /// </summary>
+    /// <param name="statusCode">The HTTP status code.</param>
+    /// <returns>Returns true if the status code is between 400 and 499.</returns>
+    let is4xxStatusCode (statusCode : int) =
+        400 <= statusCode && statusCode <= 499
+
+    /// <summary>
+    /// Utility function for matching 5xx HTTP status codes.
+    /// </summary>
+    /// <param name="statusCode">The HTTP status code.</param>
+    /// <returns>Returns true if the status code is between 500 and 599.</returns>
+    let is5xxStatusCode (statusCode : int) =
+        500 <= statusCode && statusCode <= 599
+
