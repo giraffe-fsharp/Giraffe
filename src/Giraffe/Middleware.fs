39c142451f0f95d09017ea9944ac231543d2150f
Esben Bjerre <ebj@in-commodities.com>
2023-10-14 13:39:44 +0200

    feat: deprecate newtonsoft

--- src/Giraffe/Middleware.fs
+++ src/Giraffe/Middleware.fs
@@ -115,8 +115,8 @@ type ServiceCollectionExtensions() =
     [<Extension>]
     static member AddGiraffe(svc : IServiceCollection) =
         svc.TryAddSingleton<RecyclableMemoryStreamManager>(fun _ -> RecyclableMemoryStreamManager())
-        svc.TryAddSingleton<Json.ISerializer>(fun sp ->
-            NewtonsoftJson.Serializer(NewtonsoftJson.Serializer.DefaultSettings, sp.GetService<RecyclableMemoryStreamManager>()) :> Json.ISerializer)
+        svc.TryAddSingleton<Json.ISerializer>(fun _ ->
+            SystemTextJson.Serializer(SystemTextJson.Serializer.DefaultOptions) :> Json.ISerializer)
         svc.TryAddSingleton<Xml.ISerializer>(fun sp ->
             SystemXml.Serializer(SystemXml.Serializer.DefaultSettings, sp.GetService<RecyclableMemoryStreamManager>()) :> Xml.ISerializer)
         svc.TryAddSingleton<INegotiationConfig, DefaultNegotiationConfig>()
