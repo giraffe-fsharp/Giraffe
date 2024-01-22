01abd920a2df6ecdc7adaafabe6767ee00e51da2
fpellet <git@florentpellet.com>
2024-01-22 15:34:48 +0100

    Fix Microsoft.IO.RecyclableMemoryStream upgrade

--- src/Giraffe/Xml.fs
+++ src/Giraffe/Xml.fs
@@ -38,7 +38,7 @@ module SystemXml =
 
         interface Xml.ISerializer with
             member __.Serialize (o : obj) =
-                use stream = if rmsManager.ThrowExceptionOnToArray then new MemoryStream() else rmsManager.GetStream("giraffe-xml-serialize")
+                use stream = if rmsManager.Settings.ThrowExceptionOnToArray then new MemoryStream() else rmsManager.GetStream("giraffe-xml-serialize")
                 use writer = XmlWriter.Create(stream, settings)
                 let serializer = XmlSerializer(o.GetType())
                 serializer.Serialize(writer, o)
