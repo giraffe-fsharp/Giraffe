e13bd2169e041348af470688375d965d8c9af46d
64J0 <vinigaio97@gmail.com>
2024-05-09 22:17:20 -0300

    tests: add new tests to EndpointRouting

---
+++ tests/Giraffe.Tests/EndpointRoutingTests.fs
@@ -0,0 +1,52 @@
+module Giraffe.Tests.EndpointRoutingTests
+
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Xunit
+open Giraffe
+open Giraffe.EndpointRouting
+open System.Net.Http
+
+// ---------------------------------
+// routef Tests
+// ---------------------------------
+
+[<Theory>]
+[<InlineData("00000000-0000-0000-0000-0000000000000", "Not Found")>]
+[<InlineData("00000000-0000-0000-0000-000000000000", "Success: 00000000-0000-0000-0000-000000000000")>]
+[<InlineData("00000000-0000-0000-0000-00000000000", "Not Found")>]
+[<InlineData("8b3557db-fa-c0c90785ec0b", "Not Found")>]
+[<InlineData("8b3557db-fa-c0c90785ec", "Success: e7f9bdf1-5bb7-f6f9-be73-473dd3bf3979")>]
+[<InlineData("8b3557db-fa-c0c90785e", "Not Found")>]
+[<InlineData("does-not-make-sense", "Not Found")>]
+[<InlineData("1", "Not Found")>]
+let ``routef: GET "/try-a-guid/%O" returns "Success: ..."`` (potentialGuid: string, expected: string) =
+    task {
+        let endpoints: Endpoint list =
+            [ GET
+                  [ route "/" (text "Hello World")
+                    route "/foo" (text "bar")
+                    routef "/try-a-guid/%O" (fun (guid: Guid) -> text $"Success: {guid}") ] ]
+
+        let notFoundHandler = "Not Found" |> text |> RequestErrors.notFound
+
+        let configureApp (app: IApplicationBuilder) =
+            app.UseRouting()
+               .UseGiraffe(endpoints)
+               .UseGiraffe(notFoundHandler)
+
+        let configureServices (services: IServiceCollection) =
+            services.AddRouting().AddGiraffe() |> ignore
+
+        let request = 
+            createRequest HttpMethod.Get $"/try-a-guid/{potentialGuid}"
+
+        let! response = 
+            makeRequest (fun () -> configureApp) configureServices () request
+
+        let! content = 
+            response |> readText
+
+        content |> shouldEqual expected
+    }
