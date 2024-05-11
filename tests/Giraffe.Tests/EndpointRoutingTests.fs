b57fac833b4cb78b9524f1c9a1ee267d765e6ac2
64J0 <vinigaio97@gmail.com>
2024-05-11 10:12:21 -0300

    tests: add ShortGuid tests

--- tests/Giraffe.Tests/EndpointRoutingTests.fs
+++ tests/Giraffe.Tests/EndpointRoutingTests.fs
@@ -16,8 +16,10 @@ open System.Net.Http
 [<InlineData("00000000-0000-0000-0000-0000000000000", "Not Found")>]
 [<InlineData("00000000-0000-0000-0000-000000000000", "Success: 00000000-0000-0000-0000-000000000000")>]
 [<InlineData("00000000-0000-0000-0000-00000000000", "Not Found")>]
+[<InlineData("0000000000000000000000", "Success: d3344dd3-344d-4dd3-34d3-4d34d34d34d3")>] // ShortGuid
+[<InlineData("DBvYFN7y#$@u933Kc8pM#^", "Not Found")>]
 [<InlineData("8b3557db-fa-c0c90785ec0b", "Not Found")>]
-[<InlineData("8b3557db-fa-c0c90785ec", "Success: e7f9bdf1-5bb7-f6f9-be73-473dd3bf3979")>]
+[<InlineData("8b3557db-fa-c0c90785ec", "Success: e7f9bdf1-5bb7-f6f9-be73-473dd3bf3979")>] // ShortGuid
 [<InlineData("8b3557db-fa-c0c90785e", "Not Found")>]
 [<InlineData("does-not-make-sense", "Not Found")>]
 [<InlineData("1", "Not Found")>]
