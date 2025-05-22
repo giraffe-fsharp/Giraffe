db434feadcd1adf8e89975c9424bb3d75341f37b
Reuben J. Sonnenberg <RJSonnenberg@users.noreply.github.com>
2025-05-22 16:45:36 +0000

    docs: add handler for petId endpoint to sample

--- samples/EndpointRoutingApp/Program.fs
+++ samples/EndpointRoutingApp/Program.fs
@@ -18,6 +18,10 @@ let handler2 (firstName: string, age: int) : HttpHandler =
 let handler3 (a: string, b: string, c: string, d: int) : HttpHandler =
     fun (_: HttpFunc) (ctx: HttpContext) -> sprintf "Hello %s %s %s %i" a b c d |> ctx.WriteTextAsync
 
+let handlerNamed (petId: int) : HttpHandler =
+    fun (_: HttpFunc) (ctx: HttpContext) ->
+        sprintf "PetId: %i" petId |> ctx.WriteTextAsync
+
 let endpoints =
     [
         subRoute "/foo" [ GET [ route "/bar" (text "Aloha!") ] ]
@@ -25,6 +29,7 @@ let endpoints =
             route "/" (text "Hello World")
             routef "/%s/%i" handler2
             routef "/%s/%s/%s/%i" handler3
+            routef "/pet/%i:petId" handlerNamed
         ]
         GET_HEAD [
             route "/foo" (text "Bar")
