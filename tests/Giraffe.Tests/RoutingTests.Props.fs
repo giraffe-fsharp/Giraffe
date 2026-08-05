2322bff1e64d0d7ab601c93f493e797170309a9c
64J0 <vinigaio97@gmail.com>
2026-08-05 19:56:38 -0300

    Add more property tests

--- tests/Giraffe.Tests/RoutingTests.Props.fs
+++ tests/Giraffe.Tests/RoutingTests.Props.fs
@@ -20,13 +20,13 @@ module Utils =
         static member ShortGuid() =
             ArbMap.defaults
             |> ArbMap.arbitrary<Guid>
-            |> Arb.convert Giraffe.ShortGuid.fromGuid Giraffe.ShortGuid.toGuid
+            |> Arb.convert ShortGuid.fromGuid ShortGuid.toGuid
 
     type GiraffeShortId =
         static member ShortId() =
             ArbMap.defaults
             |> ArbMap.arbitrary<uint64>
-            |> Arb.convert Giraffe.ShortId.fromUInt64 Giraffe.ShortId.toUInt64
+            |> Arb.convert ShortId.fromUInt64 ShortId.toUInt64
 
     type GiraffeFloat =
         static member Float() =
@@ -48,16 +48,35 @@ module Utils =
         GET
         >=> choose [
             route "/" >=> text "Hello World"
+            routeCi "/hello-ci" >=> text "CI works"
             routef "/is-valid/%b" (fun isValid -> text (sprintf "IsValid: %b" isValid))
             routef "/char/%c" (fun char -> text (sprintf "Char: %c" char))
             routef "/name/%s" (fun name -> text (sprintf "Name: %s" name))
             routef "/age/%i" (fun age -> text (sprintf "Age: %i" age))
+            routef "/big-age/%d" (fun (age: int64) -> text (sprintf "BigAge: %d" age))
             routef "/price/%f" (fun price -> text (sprintf "Price: %f" price))
             routef "/guid/%O" (fun (guid: Guid) -> text (sprintf "GUID: %O" guid))
             routef "/short-guid/%s" (fun shortGuid -> text (sprintf "Short GUID: %O" shortGuid))
             routef "/short-id/%s" (fun shortId -> text (sprintf "Short ID: %O" shortId))
+            routef "/combo/%s/%i" (fun (name, age) -> text (sprintf "Combo: %s is %i" name age))
             setStatusCode 404 >=> text "Not found"
         ]
+
+    /// Re-cases every character of `s` according to `flags`, cycling through
+    /// `flags` when it is shorter than `s`. Used to exercise case-insensitive
+    /// route matching with arbitrary casing.
+    let toggleCase (flags: bool[]) (s: string) =
+        if flags.Length = 0 then
+            s
+        else
+            String.mapi
+                (fun i c ->
+                    if flags.[i % flags.Length] then
+                        Char.ToUpperInvariant c
+                    else
+                        Char.ToLowerInvariant c
+                )
+                s
 
 [<Property>]
 let ``routef: GET "/is-valid/%b" works`` (x: bool) =
@@ -203,3 +222,100 @@ let ``routef: GET "/short-id/%s" works`` (x: string) =
         | None -> assertFailf "Result was expected to be %s" expected
         | Some ctx -> Assert.Equal(expected, getBody ctx)
     }
+
+[<Property>]
+let ``routef: GET "/big-age/%d" works`` (x: int64) =
+    let ctx = Substitute.For<HttpContext>()
+
+    ctx.Request.Method.ReturnsForAnyArgs "GET" |> ignore
+
+    ctx.Request.Path.ReturnsForAnyArgs(PathString(sprintf "/big-age/%d" x))
+    |> ignore
+
+    ctx.Response.Body <- new MemoryStream()
+    let expected = sprintf "BigAge: %d" x
+
+    task {
+        let! result = Utils.app next ctx
+
+        match result with
+        | None -> assertFailf "Result was expected to be %s" expected
+        | Some ctx -> Assert.Equal(expected, getBody ctx)
+    }
+
+[<Property(Arbitrary = [| typeof<Utils.GiraffeString> |])>]
+let ``routef: GET "/combo/%s/%i" works with multiple captured arguments`` (name: string, age: int) =
+    let ctx = Substitute.For<HttpContext>()
+
+    ctx.Request.Method.ReturnsForAnyArgs "GET" |> ignore
+
+    ctx.Request.Path.ReturnsForAnyArgs(PathString(sprintf "/combo/%s/%i" name age))
+    |> ignore
+
+    ctx.Response.Body <- new MemoryStream()
+    let expected = sprintf "Combo: %s is %i" name age
+
+    task {
+        let! result = Utils.app next ctx
+
+        match result with
+        | None -> assertFailf "Result was expected to be %s" expected
+        | Some ctx -> Assert.Equal(expected, getBody ctx)
+    }
+
+[<Property>]
+let ``routeCi: GET "/hello-ci" works regardless of casing`` (flags: bool[]) =
+    let ctx = Substitute.For<HttpContext>()
+    let path = Utils.toggleCase flags "/hello-ci"
+
+    ctx.Request.Method.ReturnsForAnyArgs "GET" |> ignore
+    ctx.Request.Path.ReturnsForAnyArgs(PathString path) |> ignore
+    ctx.Response.Body <- new MemoryStream()
+    let expected = "CI works"
+
+    task {
+        let! result = Utils.app next ctx
+
+        match result with
+        | None -> assertFailf "Result was expected to be %s" expected
+        | Some ctx -> Assert.Equal(expected, getBody ctx)
+    }
+
+[<Property(Arbitrary = [| typeof<Utils.GiraffeString> |])>]
+let ``routef: GET on an unmatched path always falls through to 404`` (suffix: string) =
+    let ctx = Substitute.For<HttpContext>()
+    let path = sprintf "/does-not-exist/%s" suffix
+
+    ctx.Request.Method.ReturnsForAnyArgs "GET" |> ignore
+    ctx.Request.Path.ReturnsForAnyArgs(PathString path) |> ignore
+    ctx.Response.Body <- new MemoryStream()
+
+    task {
+        let! result = Utils.app next ctx
+
+        match result with
+        | None -> assertFail "Result was expected to be Some ctx with a 404 response"
+        | Some ctx ->
+            Assert.Equal(404, ctx.Response.StatusCode)
+            Assert.Equal("Not found", getBody ctx)
+    }
+
+module ShortGuidProps =
+
+    [<Property(Arbitrary = [| typeof<Utils.GiraffeGuid> |])>]
+    let ``ShortGuid: fromGuid >> toGuid roundtrips`` (guid: Guid) =
+        guid |> ShortGuid.fromGuid |> ShortGuid.toGuid = guid
+
+    [<Property(Arbitrary = [| typeof<Utils.GiraffeGuid> |])>]
+    let ``ShortGuid: fromGuid always produces a 22 character string`` (guid: Guid) =
+        (ShortGuid.fromGuid guid).Length = 22
+
+module ShortIdProps =
+
+    [<Property>]
+    let ``ShortId: fromUInt64 >> toUInt64 roundtrips`` (id: uint64) =
+        id |> ShortId.fromUInt64 |> ShortId.toUInt64 = id
+
+    [<Property>]
+    let ``ShortId: fromUInt64 always produces an 11 character string`` (id: uint64) =
+        (ShortId.fromUInt64 id).Length = 11
